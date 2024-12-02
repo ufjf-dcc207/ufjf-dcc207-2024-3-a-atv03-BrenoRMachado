697351d4bd796291fd2fd4ca069bdabf5b07310f
Breno Machado <burracha.6464@gmail.com>
2024-12-02 20:49:07 -0300

    adiciona mais animais

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import Animal from './Animal'
 function App() {
   return (
     <div className ="app">
-      <Animal icone='🦁' nome='Leão' peso={-1} extincao = {true}/>
+      <Animal icone='🦁' nome='Leão' peso={-1} extincao/>
+      <Animal icone='🦒' nome='Girafa' peso={1200} extincao/>
+      <Animal icone='🦜' nome='Paraguaio' peso={0.12} extincao/>
     </div>
   );
 }
