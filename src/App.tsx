e085f23df3590bfcc407273b25fd588617aea047
Breno Machado <burracha.6464@gmail.com>
2024-12-02 19:50:49 -0300

    adiciona animal no app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import Animal from './Animal'
 import './App.css'
 
 function App() {
-  return <div className ='app'>Oi Mundo</div>
+  return <div className ="app">
+    <Animal/>
+  </div>
 }
 
 export default App
