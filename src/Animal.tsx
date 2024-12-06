1d57dad27f33e6e4326cd89379e458e6e689dc66
Breno Machado <burracha.6464@gmail.com>
2024-12-05 21:49:08 -0300

    adiciona exibição

--- src/Animal.tsx
+++ src/Animal.tsx
@@ -13,7 +13,11 @@ export default function({icone, nome, peso , extincao} : AnimalProps)
     <div className="animal">
         <div className="icone">{icone}</div>
         <div className="nome">{nome}</div>
-        {peso >0.0 ? <div className="peso">{peso.toLocaleString()}kg</div> : <div className="peso">Desconhecido!</div>}
+        {peso >0.0 ? (
+            <div className="peso">{peso.toLocaleString()}kg</div> 
+            ) : (
+                <div className="peso">Desconhecido!</div> 
+            )}
         
         {extincao && <div className="extincao">Em extinção!</div>}
     </div>
